5ad59eaeaf82b0e27678aa2fd080cf70583106bc
Renzo Siza Tejada <renzosiza7@gmail.com>
2020-07-19 11:45:29 -0500

    Add controllers for errors, solutions and links

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+//Errors
+Route::get('/error/{id}', 'ErrorController@index')->name('error');
